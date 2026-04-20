4a1d89033cbe17eac57d6343c6d1719077435a33
Aitor Pazos <mail@aitorpazos.es>
2026-04-20 11:49:33 +0100

    build: bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.telesor"
         minSdk = 35
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
