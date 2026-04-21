ebaf97b538d1e9982b3d79f85107187cab754a54
Aitor Pazos <mail@aitorpazos.es>
2026-04-21 14:29:39 +0100

    build: bump version to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.telesor"
         minSdk = 31
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.5.0"
     }
 
     buildTypes {
