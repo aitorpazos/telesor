1102e7786148eaec1263c078fc548200dd4e60ef
Aitor Pazos <mail@aitorpazos.es>
2026-04-21 13:27:53 +0100

    build: bump version to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.telesor"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
     }
 
     buildTypes {
